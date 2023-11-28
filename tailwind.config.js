bc21b4c6fe1aa7673cf83130063b1f154b7aabd0
Avi <aviblinder@gmail.com>
2023-11-28 12:04:45 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      extend: {
+        brightness: {
+          113: '1.13',
+        },
+        contrast: {
+          113: '1.13',
+        },
+        saturate: {
+          120: '1.2',
+        },
+        sepia: {
+          8: '.08',
+        },
       },
     },
   },
   plugins: [],
-}
+};
