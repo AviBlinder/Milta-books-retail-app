3976b737647c7f71ea29c16a9215798f3fad1712
Avi <aviblinder@gmail.com>
2023-11-30 14:15:02 +0200

    Navbar style

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -101,12 +101,15 @@ export default function Navbar() {
       {/* Mobile Menu */}
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="lg:hidden py-3 px-6"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-bridal-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+        <div className="fixed inset-0 z-50 " />
+        <Dialog.Panel
+          className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-bridal-900 
+        px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+        >
           <div className="flex items-center justify-between ">
             <Link
               to="Home"
