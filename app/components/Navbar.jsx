0b499fe3721d7a6a7afba6cdb7f1f166b36577cf
Avi <aviblinder@gmail.com>
2023-12-02 16:07:33 +0200

    navbar on mobile styling adjusted

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -63,9 +63,8 @@ export default function Navbar() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="mr-2.5 inline-flex items-center justify-center p-2.5 text-gray-400
-            bg-white rounded-full
-            "
+            className="ml-2 mr-2 inline-flex items-center justify-center p-2.5 text-gray-400
+            bg-bridal-900 rounded-full"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -98,17 +97,17 @@ export default function Navbar() {
       {/* Mobile Menu */}
       <Dialog
         as="div"
-        className="lg:hidden py-3 px-6"
+        className="lg:hidden"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-50 " />
         <Dialog.Panel
           className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-bridal-900 
-        px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
+        px-4 sm:max-w-sm sm:ring-1 sm:ring-white/10"
         >
           <div className="flex items-center justify-between ">
-            <Link href="/" className="-m-1.5 p-4">
+            <Link href="/" className="-m-2 p-4">
               <span className="sr-only">Milta Books</span>
               <img
                 className="h-12 w-auto rounded-full"
@@ -118,14 +117,14 @@ export default function Navbar() {
             </Link>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-600"
+              className="-m-2 rounded-md p-4 text-gray-600"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="flow-root">
             <div className="-my-6 divide-y divide-gray-500/25">
               <div className="space-y-2 py-6 flex flex-col items-center">
                 {navigation.map((item, index) => (
