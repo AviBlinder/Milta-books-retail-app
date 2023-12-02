cfc15269a5c880101c46a053caf1cb69ab9e0dda
Avi <aviblinder@gmail.com>
2023-12-02 22:27:55 +0200

    removed unused components

--- app/page.js
+++ app/page.js
@@ -1,10 +1,4 @@
-'use client';
 import LandingPage from '@/app/components/LandingPage';
-import RecommendedBooks from '@/app/components/RecommendedBooks';
-import Events from '@/app/components/Events';
-import About from '@/app/components/About';
-import WeeklyPoem from '@/app/components/WeeklyPoem';
-import { usePathname } from 'next/navigation';
 
 export default function Landing() {
   return (
